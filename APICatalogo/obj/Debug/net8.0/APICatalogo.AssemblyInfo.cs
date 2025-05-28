c494c3b7404e41af5f511d036ad629d8457c511f
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-05-27 23:54:53 -0300

    Feito migration

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3dfc43dd660a29b5d55deacfa05d42d56dd504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79a77d60cca7c81b7ed95466265e7a5c28b1701")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
