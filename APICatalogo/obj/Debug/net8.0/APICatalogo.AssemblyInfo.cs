a672bf7136dde69d832ae375afeb3e17003ebb5e
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-05-27 22:53:03 -0300

    Adicionado modelos: Categorias e Produtos

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f96fc5d8e24ac5fb83a5ad91a118467198dc1c96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913fb3ffb659de0c5f01b3d1915b2cd6cd2be2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
