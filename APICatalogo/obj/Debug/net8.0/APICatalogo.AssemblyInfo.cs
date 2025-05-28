6c654395c7538c928d3e8a0dfd981004409515c7
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-05-27 23:37:02 -0300

    Adicionado string de conexao com bd

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a672bf7136dde69d832ae375afeb3e17003ebb5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3dfc43dd660a29b5d55deacfa05d42d56dd504")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
