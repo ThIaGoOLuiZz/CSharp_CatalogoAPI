e83835ba21d539bcb4f7286567de6b75c2f8223d
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-05-29 23:13:09 -0300

    Adicionado POST para inserir novo produto

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c5f26668105e65ee3fdd7a72ba4b20fd579295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ad461a46e6f4e7bb0052734e5756ec971c8f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
