e6688e7028854d9c87f8b2b07e8e9a5cbf28534b
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-05-29 22:46:46 -0300

    Criado controladora da classe Produtos

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be798842ad03a5d476e9c383852d1daddb451807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca1f1820322a85b6bcea4331f745b422ff36889")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
